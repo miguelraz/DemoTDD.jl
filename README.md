fa3cbf9134f6e18a6c6e8c80048a70ff207df4ec
Miguel Raz Guzmán Macedo <miguelraz@gmail.com>
2021-09-08 22:42:27 -0500

    agregar README

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # DemoTDD
+
+Esta repo existe para enseñar los códigos que estaré demostrando sobre cómo usar JuliaLang en el live de 
+Manuel Gil.
+
+![](https://www.youtube.com/watch?v=HxKVFPU-TYk)
+
+Recuerden que pueden descargar Julia desde julialang.org y empezar a jugar.
+
+Happy hacking!
+
+### Links
+
+[JuliaCon2021](https://juliacon.org/2021/) - conferencia en línea, gratis, y todo en YouTube, [https://www.youtube.com/watch?v=HEVOgSLBzWA]()
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,36 @@ using Test
 
 @testset "DemoTDD.jl" begin
     # Write your tests here.
+
+    @test 3 == 3
+	# Super cool y nuevo proceso:
+	#@test T == T broken=(T = NaN)
+    #
+    @test_broken 1 == 2
+
+    @testset "Nivel 1" begin
+        @test 1 == 1
+        @testset "Nivel 2" begin
+            @test 2 == 2
+            @testset "Nivel 3" begin
+                @test 3 == 2
+            end
+        end
+    end
+    for i in (1,2,3)
+        @test i == i
+    end
+
+    @testset "Nivel 1 otra vez" begin
+        @test true
+        @test false
+        @test true
+    end
+
+    @test all(true for i in 1:100000)
+
+    @test_throws BoundsError [1, 2][3]
+
+
+
 end
